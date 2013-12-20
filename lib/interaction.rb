9ed0086ccbf24658fc8021330778a3e84d6bf0b7
evilemons <stefanbay@me.com>
2013-12-19 22:54:25 -0500

    Add support for colors.

--- lib/interaction.rb
+++ lib/interaction.rb
@@ -3,10 +3,19 @@
 require_relative "parser.rb"
 
 class Interaction
+	public
+	def mkgreen(text); colorize(text, 32);end
+	def mkblue(text); colorize(text, 34);end
+	def mkred(text); colorize(text, 31);end
+	def mkpurple(text); colorize(text, 35);end
 
 	private
 	def initialize(room)
 		command = gets.chomp
 		Parser.new.parse(command, room)
 	end
+	def colorize(text, color)
+		"\e[#{color_code}m#{text}\e[0m"
+	end
+
 end
